fa6f13148634c439e3d65e60cf909fe48dbbc942
Felipe-Takayuki <felipe.takahashi@etec.sp.gov.br>
2025-04-25 12:35:13 -0300

    feat: fazendo o componente de card

---
+++ src/app/components/card/card.component.ts
@@ -0,0 +1,14 @@
+import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-card',
+  imports: [CommonModule],
+  templateUrl: './card.component.html',
+  styleUrl: './card.component.css'
+})
+export class CardComponent {
+  @Input() numero_termo:string = ""
+  @Input() background: string = ""
+
+}
