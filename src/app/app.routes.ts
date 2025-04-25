b160fbb7bfef0c6b0656a3593fe34ea4f6c36522
Felipe-Takayuki <felipe.takahashi@etec.sp.gov.br>
2025-04-25 12:35:33 -0300

    configuração de rotas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { RegisterComponent } from './pages/register/register.component';
+import { LoginComponent } from './pages/login/login.component';
+import { HomeComponent } from './pages/home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: "register", component: RegisterComponent
+    },
+    {
+        path: "", component: LoginComponent
+    },
+    {
+        path: "home", component: HomeComponent
+    }
+];
