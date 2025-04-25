6b8f0dcd35778ea2f49072687cf55dda2acb362f
Felipe-Takayuki <felipe.takahashi@etec.sp.gov.br>
2025-04-25 12:30:53 -0300

    feat: fazendo a tela de registro

---
+++ src/app/pages/register/register.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { ButtonComponent } from '../../components/button/button.component';
+
+@Component({
+  selector: 'app-register',
+  imports: [ButtonComponent],
+  templateUrl: './register.component.html',
+  styleUrl: './register.component.css'
+})
+export class RegisterComponent {
+
+}
